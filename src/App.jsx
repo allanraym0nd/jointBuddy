cf02cd2f148daa59990771f50b8fc31cb3af219a
allan raymond <raymonda916@gmail.com>
2025-06-23 05:32:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import Header from './components/common/Header'
-
+import MapContainer from './components/map/MapContainer'
 
 function App() {
   
   return (
       <div className="h-screen flex flex-col">
       <Header />
+
+      <main className="flex-1 flex">
+        <div className="flex-1">
+          <MapContainer />
+        </div>
+          </main>
       </div>
   
   )
