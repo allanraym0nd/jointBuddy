58394cfb82ac8087114457bb5219bcb63741a374
allan raymond <raymonda916@gmail.com>
2025-06-23 05:32:05 +0300

    Update MapContainer component

--- src/components/map/MapContainer.jsx
+++ src/components/map/MapContainer.jsx
@@ -1,4 +1,4 @@
-// import LoadingSpinner; 
+import LoadingSpinner from "../common/LoadingSpinner";
 import { useState, useEffect } from "react";
 
 const MapContainer = () => {
@@ -10,13 +10,14 @@ setTimeout(()=> setIsLoading(false), 2000)
 
     if(isLoading){
         return (
-            <LoadingSpinner size='lg' text='Loading Map...'/>
+            <LoadingSpinner size='lg'/>
         )
     }
 
-    return (
-        <div>
-
-        </div>
-    )
+   return (
+    <div className="w-full h-full bg-blue-50 flex items-center justify-center"> 
+    </div>
+  )
 }
+
+export default MapContainer;
