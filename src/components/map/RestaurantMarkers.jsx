19ec873ebd070b613f15bf8ddb5af24b2da20d49
allan raymond <raymonda916@gmail.com>
2025-07-02 18:26:13 +0300

    Update RestaurantMarkers component

--- src/components/map/RestaurantMarkers.jsx
+++ src/components/map/RestaurantMarkers.jsx
@@ -1,19 +1,18 @@
-
-import { RestaurantMarker } from "./RestaurantMarker";
+import RestaurantMarker from "./RestaurantMarker";  // ← Fixed import
 
 const RestaurantMarkers = ({restaurants, map, onRestaurantClick}) => {
-
     return (
         <>
-     {restaurants.map((restaurant)=> {
-        <RestaurantMarker 
-        onMarkerClick = {onRestaurantClick}
-        restaurant = {restaurant}
-        key = {restaurant.id}
-        map={map}
-        />
-     })}
-     </>
-   
+         {restaurants.map((restaurant) => (  // ← Added parentheses for implicit return
+            <RestaurantMarker 
+                onMarkerClick={onRestaurantClick}
+                restaurant={restaurant}
+                key={restaurant.id}
+                map={map}
+            />
+         ))}
+         </>
     )
 }
+
+export default RestaurantMarkers;
