067fda1fde4505533ed570521985f8b86f43a59e
allan raymond <raymonda916@gmail.com>
2025-07-05 17:30:27 +0300

    took out header

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
-import Header from './components/common/Header'
+// import Header from './components/common/Header'
 import MapContainer from './components/map/MapContainer'
 
 function App() {
   
   return (
-      <div className="h-screen flex flex-col">
-      <Header />
+      // <div className="h-screen flex flex-col">
+      // <Header />
 
       <main className="flex-1 flex">
         <div className="flex-1">
           <MapContainer />
         </div>
           </main>
-      </div>
+      // </div>
   
   )
 }
